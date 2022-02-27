520972bcd68216d1bf5d49c257419ae229deb1c5
codigoenlaweb <codeenlaweb@gmail.com>
2022-02-27 17:29:26 -0400

    finish

---
+++ README.md
@@ -0,0 +1,22 @@
+##Made with the qrcode.js library
+
+compatible with desktop and phone browsers
+
+##Built with
+js and tailwind css
+
+##Project description
+It is a Single-page application where you can generate QR codes in a simple way and with the possibility of changing the color of the QR code
+
+**types of generated QR codes**
+-  url
+- email
+- text
+
+##Author
+
+Jesús olmos (codigoenlaweb)
+
+##Link to the page
+
+[http://generatorQR](http://generatorQR "http://generatorQR")
--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,7 @@ const links = document.querySelectorAll("#link");
 const removeAndAgredInput = document.querySelector("#remove-and-agred-input");
 const pError = document.querySelector("#error");
 const color = document.querySelector("#color");
+const downloadImage = document.querySelector("#download-image");
 
 let qrcode = new QRCode(divQrcode, {
   width: 240,
@@ -14,19 +15,37 @@ let qrcode = new QRCode(divQrcode, {
   correctLevel: QRCode.CorrectLevel.H,
 });
 
-btn.addEventListener("click", () => {
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
   inputs.forEach((input) => {
     if (!input.classList.contains("hidden")) {
       const inputValue = input.value;
       if (inputValue === "") {
         pError.classList.remove("hidden");
+        setTimeout(() => {
+          pError.classList.remove("opacity-0");
+        }, 0);
       } else {
-        qrcode._htOption.colorDark = color.value
+        qrcode._htOption.colorDark = color.value;
         qrcode.clear(); // clear the code.
         qrcode.makeCode(inputValue); // make another code.
         if (!pError.classList.contains("hidden")) {
-          pError.classList.add("hidden");
+          pError.classList.add("opacity-0");
+          setTimeout(() => {
+            pError.classList.add("hidden");
+          }, 300);
         }
+
+        setTimeout(() => {
+          const img = divQrcode.childNodes[1].src
+          downloadImage.href = img
+        }, 10);
+
+        downloadImage.classList.remove("hidden")
+        downloadImage.classList.remove("block")
+        setTimeout(() => {
+          downloadImage.classList.remove("opacity-0")
+        }, 0);
       }
     }
   });
@@ -35,10 +54,18 @@ btn.addEventListener("click", () => {
 function insertInput(nmr) {
   inputs.forEach((input) => {
     if (input === inputs[nmr]) {
-      input.classList.remove("hidden");
+      setTimeout(() => {
+        input.classList.remove("hidden");
+        setTimeout(() => {
+          input.classList.remove("opacity-0");
+        }, 1);
+      }, 300);
     } else {
-      input.classList.add("hidden");
-      input.value = "";
+      input.classList.add("opacity-0");
+      setTimeout(() => {
+        input.classList.add("hidden");
+        input.value = "";
+      }, 300);
     }
   });
 }
